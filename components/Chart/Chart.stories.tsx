e722327ed43d60d6fd673875da08bbfa68540212
Eddie Ornelas <eddiie.ornelas@gmail.com>
2025-12-30 17:51:39 -0800

    Fix lint issues

--- components/Chart/Chart.stories.tsx
+++ components/Chart/Chart.stories.tsx
@@ -118,7 +118,7 @@ export const CustomRender1: Story = {
         .append("g")
         .attr(
           "transform",
-          `translate(${ctx.innerWidth / 2},${ctx.innerHeight / 2})`
+          `translate(${ctx.innerWidth / 2},${ctx.innerHeight / 2})`,
         );
 
       const colorScale = [
