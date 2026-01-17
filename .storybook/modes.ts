693acb485b0b1b627015ce6c7792b352ce99a4ae
Eddie Ornelas <eddiie.ornelas@gmail.com>
2026-01-17 11:53:08 -0800

    Adds themes to mode so visual testing is done across all themes we support (#69)

---
+++ .storybook/modes.ts
@@ -0,0 +1,14 @@
+export const allModes = {
+  Default: {
+    theme: "default",
+  },
+  Doomsday: {
+    theme: "doom",
+  },
+  Captain: {
+    theme: "captain",
+  },
+  Vigilante: {
+    theme: "vigilante",
+  },
+} as const;
